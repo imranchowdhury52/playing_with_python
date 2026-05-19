85abaf6f538b396568b1e5550c7f45a2aeb96562
imranchowdhury52 <33127185+imranchowdhury52@users.noreply.github.com>
2026-05-19 21:20:00 +0200

    Added even odd number analyzer

---
+++ basics/even_odd_analyzer.py
@@ -0,0 +1,17 @@
+'''
+Probelm: Even/Odd Analyzer
+
+1. Takes a number from the user
+2. Checks whether the number is:
+Even
+Odd
+
+'''
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(number, "is Even")
+
+else:
+    print(number, "is Odd")   
