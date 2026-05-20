362a0c01879c493e93f771a523ee2b8c1a96659b
imranchowdhury52 <33127185+imranchowdhury52@users.noreply.github.com>
2026-05-20 22:39:00 +0200

    updated even odd number analyzer with negative and zero logic

--- basics/even_odd_analyzer.py
+++ basics/even_odd_analyzer.py
@@ -6,12 +6,42 @@ Probelm: Even/Odd Analyzer
 Even
 Odd
 
+3. Also checks:
+
+if the number is positive
+negative
+or zero
+
 '''
 
 number = int(input("Enter a number: "))
 
-if number % 2 == 0:
-    print(number, "is Even")
 
-else:
-    print(number, "is Odd")   
+if number > 0:
+
+    if number % 2 == 0:
+        print(number, "is Even")
+            
+    else:
+        print(number, "is Odd") 
+
+    print(number, "is Postive")
+
+elif number < 0:
+    if number % 2 == 0:
+        print(number, "is Even")
+            
+    else:
+        print(number, "is Odd") 
+
+    print(number, "is Negative")
+
+elif number == 0:
+    if number % 2 == 0:
+        print(number, "is Even")
+            
+    else:
+        print(number, "is Odd") 
+
+    print(number, "is Zero")
+
